7887374b09a2f233080bf38621a9669a2e43a333
Jesse Rodrigues <122498927+jessedatawiz@users.noreply.github.com>
2024-08-31 20:55:44 +0000

    frech deck code

---
+++ study_codes/french_deck_20240831.py
@@ -0,0 +1,38 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+deck = FrenchDeck()
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+# bitwise shift
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return (rank_value << 2) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high): 
+    print(card)
+
+
+#print(len(deck))
+
+#from random import choice
+#print(choice(deck))
+
+#for card in deck:  # doctest: +ELLIPSIS
+ #   print(card)
